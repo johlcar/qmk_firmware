baaa205908d1d8ffeadd9c8278fd52d941a85bc2
johlcar <johlcar2@gmail.com>
2018-06-22 00:57:25 -0500

    update keymap

--- keyboards/atreus/keymaps/johlcar/keymap.c
+++ keyboards/atreus/keymaps/johlcar/keymap.c
@@ -16,31 +16,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_LGUI, KC_TAB,  KC_LSFT, KC_SPC,  KC_LALT, KC_LCTL, KC_BSPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
+    KC_ESC, KC_LGUI, KC_LCTL,  KC_TAB, KC_SPC,  KC_LSFT, KC_LALT, KC_BSPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
   ),
 
   /*
-   *  !       @     up     {    }        ||     pgup    7     8     9    *
-   *  #     left   down  right  $        ||     pgdn    4     5     6    +
-   *  [       ]      (     )    &        ||       `     1     2     3    \
-   * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
+   *  1       2     3     4    5         ||        6    7     8     9     0
+   *  `       [     ]     (    )         ||      left down    up  right   =
+   *  !       @     #     {    }         ||        ^    +     &     $     \
+   * lower  super ctrl  tab  spc    shft || alt  bkspc  fn   pgup  pgdn   *
    */
   [_RS] = LAYOUT( /* [> RAISE <] */
-    KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
-    KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
-    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
+    KC_1, KC_2,   KC_3,   KC_4, KC_5,                   KC_6, KC_7,    KC_8,   KC_9, KC_0 ,
+    KC_GRV, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,                    KC_LEFT, KC_DOWN,    KC_UP,   KC_RIGHT, KC_EQL ,
+    KC_EXLM, KC_AT, KC_HASH, KC_LCBR, KC_RCBR,                   KC_CIRC,  KC_PLUS,    KC_AMPR,   KC_DLR, KC_BSLS ,
+    TG(_LW), KC_LGUI,  KC_LCTL, KC_TAB, KC_SPC, KC_LSFT, KC_LALT, KC_BSPC,  KC_TRNS, KC_PGUP, KC_PGDN, KC_ASTR  ),
   /*
-   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
-   *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *       volup             reset      ||             F1    F2    F3   F12
-   *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
+   *   F1    F2   F3    F4    F5         ||      F6     F7    F8    F9   F10
+   *             home insert             ||     left    down  up  right  F11
+   *  volup       end delete  reset      ||                              F12
+   *  voldn super ctrl tab    spc  shift || alt bkspc   L0  prtsc scroll pause
    */
   [_LW] = LAYOUT( /* [> LOWER <] */
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS )
+    KC_F1,  KC_F2, KC_F3,   KC_F4,  KC_F5,                   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
+    KC_NO, KC_NO, KC_HOME, KC_INS, KC_NO,                   KC_LEFT, KC_DOWN,   KC_UP,   KC_RIGHT,   KC_F11  ,
+    KC_VOLU,   KC_NO, KC_END,   KC_DELT,   RESET,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_F12  ,
+    KC_VOLD,   KC_LGUI, KC_LCTL, KC_TAB, KC_SPC, KC_LSFT, KC_LALT, KC_BSPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS )
 };
 
 const uint16_t PROGMEM fn_actions[] = {
